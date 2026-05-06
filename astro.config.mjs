4b666fd72b819abbd857935c5d14d607e5f1c6c2
Paul <paul@Pauls-MacBook-Air.local>
2026-05-06 19:33:55 -0400

    Fix internal links to use BASE_URL for private build routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  base: process.env.BASE_PATH || undefined,
+  base: process.env.BASE_PATH ? process.env.BASE_PATH.replace(/\/?$/, '/') : undefined,
 });
